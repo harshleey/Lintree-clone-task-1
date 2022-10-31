41740dbec149f90e864bac838f1737af0a75ce4c
Wonuola <logun.fateem@gmail.com>
2022-10-31 22:29:16 +0100

    Fixed the unordered list

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Link({link}) {
   return (
      
-        <li id={link.idName} className='link'
+        <li className='link'
         >
-        <a href={`https://${link.link}`} 
+        <a id={link.idName} href={`https://${link.link}`} 
         target="_blank">{link.webName}</a></li>
       
   )
